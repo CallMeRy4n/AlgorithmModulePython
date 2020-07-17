c8a63104e067f66906c640590d6c51e752d41b91
RanQuanRyanTwo <60098085+RanQuanRyanTwo@users.noreply.github.com>
2020-07-17 11:51:40 +0700

    Setup for project

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="Algorithm-Ry4n-module",
+    version="0.0.1",
+    author="Ry4n",
+    author_email="minhquan99kk@gmail.com",
+    description="Just a module contain many algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CallMeRy4n/AlgirthmRy4nModules",
+    packages=[
+        "ry4n_module"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
